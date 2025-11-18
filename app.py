6f58a2b2f615e86f683490433ecf69736289cf0e
xiaorunkang0514-hub <xiaorunkang0514@gmail.com>
2025-11-18 03:29:24 +0000

    11.18

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
+@app.route("/main", methods=["GET","POST"])
+def main():
+    r=request.form.get("q")
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])  
+def dbs():
+    return(render_template("dbs.html"))
+
 if __name__=="__main__":
     app.run()
+
